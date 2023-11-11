6aac29d152b18c7ce89de96471b8d59521c381ad
Aditya Mohite <mohiteaditya689@gmail.com>
2023-11-11 11:37:13 +0530

    An idea from python lecture

---
+++ randomlist.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <time.h>
+#include <stdlib.h>
+using namespace std;
+class randomlist
+{
+public:
+    int array[10] = {};
+
+    void generate(){
+       for (int i = 1; i <= 10; i++)
+    {
+        array[i] = rand() % 100;
+    } 
+    }
+   
+};
+
+int main()
+{
+    //srand(time(NULL));
+    // int array[10] = {};
+
+    // for (int i = 1; i <= 10; i++)
+    // {
+    //     array[i] = rand() % 100;
+    // }
+
+    // cout << "The random list is : " << endl;
+    // for (int i = 1; i <= 10; i++)
+    // {
+    //     cout << array[i] << endl;
+    // }
+
+    // cout << "Odd numbers for the above list :" << endl;
+    // for (int i = 1; i <= 10; i++)
+    // {
+    //     if (array[i] % 2 != 0)
+    //     {
+    //         cout << array[i] << endl;
+    //     }
+    // }
+}
