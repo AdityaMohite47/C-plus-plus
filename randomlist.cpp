0e5498b5be18d66b214cb2c473529f89c2a78503
Aditya Mohite <mohiteaditya689@gmail.com>
2023-11-12 22:10:35 +0530

    new random list generator

--- randomlist.cpp
+++ randomlist.cpp
@@ -2,42 +2,47 @@
 #include <time.h>
 #include <stdlib.h>
 using namespace std;
-class randomlist
-{
-public:
-    int array[10] = {};
+// class randomlist
+// {
+// public:
+//     int array[10] = {};
 
-    void generate(){
-       for (int i = 1; i <= 10; i++)
-    {
-        array[i] = rand() % 100;
-    } 
-    }
-   
-};
+//     void generate(){
+//        for (int i = 1; i <= 10; i++)
+//     {
+//         array[i] = rand() % 100;
+//     }
+//     }
+
+// };
 
 int main()
 {
-    //srand(time(NULL));
-    // int array[10] = {};
+    srand(time(NULL));
+    int array[10] = {};
 
-    // for (int i = 1; i <= 10; i++)
-    // {
-    //     array[i] = rand() % 100;
-    // }
+    cout << "The random list is : " << endl;
+    for (int i = 1; i <= 10; i++)
+    {
+        array[i] = rand() % 100;
+        cout << array[i] << endl;
+    }
 
-    // cout << "The random list is : " << endl;
-    // for (int i = 1; i <= 10; i++)
-    // {
-    //     cout << array[i] << endl;
-    // }
+    cout << "Odd numbers for the above list :" << endl;
+    for (int i = 1; i <= 10; i++)
+    {
+        if (array[i] % 2 != 0)
+        {
+            cout << array[i] << endl;
+        }
+    }
 
-    // cout << "Odd numbers for the above list :" << endl;
-    // for (int i = 1; i <= 10; i++)
-    // {
-    //     if (array[i] % 2 != 0)
-    //     {
-    //         cout << array[i] << endl;
-    //     }
-    // }
+    cout << "Even numbers for the above list :" << endl;
+    for (int i = 1; i <= 10; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            cout << array[i] << endl;
+        }
+    }
 }
