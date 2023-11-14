03f41bee5356e5950f023fb139cf3c1b3fda1bb9
Aditya Mohite <mohiteaditya689@gmail.com>
2023-11-14 12:28:13 +0530

    14-November-2023

---
+++ PointerToDerivedClass.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+using namespace std;
+class base
+{
+public:
+    int varbase;
+    void display()
+    {
+        cout << "Displaying Base class variable varbase : " << endl
+             << varbase << endl;
+    }
+};
+
+class derived : public base
+{
+public:
+    int varderived;
+    void display()
+    {
+        cout << "Displaying Derived class variable varderived : " << endl
+             << varderived << endl;
+        cout << "Displaying Derived class variable inherited form base varbase : " << endl
+             << varbase << endl;
+    }
+};
+
+int main()
+{
+    base *basepointer; // pointer of base class.
+
+    base objbase;
+    derived objderived;
+
+    basepointer = &objderived; // here we assign pointer to the object of base class to object of derived class
+                               // it is totally valid in C++ and will not throw error.
+
+    //  Behaviour of this pointer : the pointer will point to the object of derived class but we try to call any function
+    //                             of derived class the same function of base class will be invoked because the pointer is  of base class.
+
+    //                       Ex. if I try to call display() of objderived from derived class the display() of base class
+    //                         will get invoked again because the pointer is of base class but it points to derived class.
+    //                          This is known as late binding. i.e Runtime Polymorphism.
+
+    //  Practically :
+    //     for base class :
+     basepointer->varbase = 69;
+     basepointer->display();
+
+    // basepointer->varderived = 96  will throw error because when we set a pointer of base class to a class derived from it then we can only
+    // access the methods and memebers of base class not of any new member or method added in derived class.
+
+    // Now if :
+
+    derived *derivedpointer;
+    derivedpointer = &objderived;
+
+    derivedpointer->varderived = 98;
+    derivedpointer->varbase = 333; // This is valid because the derived class is derived from base class so it can access varbase variable.
+    derivedpointer->display();
+}
---
+++ PolymorphismInC++.cpp
@@ -0,0 +1,23 @@
+//  Polymorphism : A object to be in many forms is said to be the polymorph object And the property is Polymorphism.
+//                 In simple way one name multiple forms...
+//                 Ex. Function overloading.
+
+// In C++ there are two types of polymorphism :
+//                    1. Compile-Time Polymorphism : 
+//                                             the object is bound to the method call in compile time (we know that which function will execute), 
+//                                             i.e which function will be called is decided in the time of compilation , Also known as Static binding 
+//                                             and Early binding.
+
+//                                             We can achieve this polymorphism through 2 ways :
+//                                                             a) Function Overloading
+//                                                             b) Operator Overloading
+                    
+//                     2. Run-Time Polymorphism : 
+//                                             the compiler delays the decision of the function call (we don't know which fuction will be called),
+//                                             i.e compiler postponds the linking of functions and arrangement of function calls and These are 
+//                                             arranged in runtime. 
+                                        
+//                                             We can achieve this polymorphism through Virtual Functions.
+
+
+
